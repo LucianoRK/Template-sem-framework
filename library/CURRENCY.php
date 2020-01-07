ce4ed8bf66b558686b62fb9755fd3868dc1fb9f2
LucianoPRK <32466727+LucianoRK@users.noreply.github.com>
2020-01-07 20:20:57 -0300

    Alteração no nome das funções

--- library/CURRENCY.php
+++ library/CURRENCY.php
@@ -2,20 +2,19 @@
 
 class CURRENCY
 {
-
-    static function moedaBrParaMysql($valor)
+    static function currencyBrForMysql($valor)
     {
         $valor2 = str_replace('.', '', $valor);
         
         return str_replace(',', '.', $valor2);
     }
 
-    static function moedaMysqlParaBr($valor)
+    static function currencyMysqlForBr($valor)
     {
         return number_format($valor, 2, ',', '.');
     }
 
-    static function converteMoedaParaBigint($valor)
+    static function converteCurrencyForBigint($valor)
     {
         $valor_final = $valor * 100;
         $valor2 = str_replace('.', '', $valor_final);
@@ -25,13 +24,13 @@ class CURRENCY
 
     static function reaisToCentavos($reais)
     {
-        return self::moedaBrParaMysql($reais) * 100;
+        return self::currencyBrForMysql($reais) * 100;
     }
 
     static function intToMysql($valor)
     {
-        $valor2 = CURRENCY::moedaMysqlParaBr($valor);
+        $valor2 = CURRENCY::currencyMysqlForBr($valor);
 
-        return CURRENCY::moedaBrParaMysql($valor2);
+        return CURRENCY::currencyBrForMysql($valor2);
     }
 }
