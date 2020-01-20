0226be85504e6d31ac891cab1deeaa96a8264660
LucianoPRK <32466727+LucianoRK@users.noreply.github.com>
2020-01-20 20:32:11 -0300

    Validador cpf

--- library/VALIDATION.php
+++ library/VALIDATION.php
@@ -29,4 +29,28 @@ class VALIDATION
             return false;
         }
     }
+
+    static function cpfvalidation($cpf) {
+
+        $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
+         
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+    
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf{$c} * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf{$c} != $d) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
